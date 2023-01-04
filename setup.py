79503e1680efa1591e73abf93c2c19cc75cc165a
Juan <jumasy1980@gmail.com>
2023-01-03 16:21:18 -0800

    [FIX] numpy layout and setup new format

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 30 04:40:21 2021
-
-@author: Juan Martinez Sykora
-"""
-
+#!/usr/bin/env python
 from setuptools import setup
 
-setup(
-      name='nm_lib',
-      version='0.0',
-      py_modules=['nm_lib'],
-      author   = 'Juan Martinez Sykora',
-      author_email = 'martinezsykora@baeri.org'#,
-      #url = 'https://gitlab.com/XXX/nm_lib_py'
-)
+setup()
